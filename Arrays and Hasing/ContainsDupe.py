f63cb26001892a28606a531a4c48de462565f504
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-05-25 20:55:32 -0700

    Contains dupe explanation

--- Arrays and Hasing/ContainsDupe.py
+++ Arrays and Hasing/ContainsDupe.py
@@ -7,3 +7,10 @@ class Solution:
             hashmap.add(i)
         
         return False
+    
+    """
+    To check if we have a duplicate in an array, we can sort the array and then see if a subsequent
+    number is the same as the former. This would be O(nlogn), because we need to sort the array (O(nlogn))
+    and then traverse the entire array (O(n)). To make this O(n), we use a set instead to keep track of 
+    numbers we've discovered. If we encounter a number in the set, then we have a duplicate and can return true.
+    """
