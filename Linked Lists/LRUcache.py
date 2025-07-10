70b8b16597e4aa46cf1311e9ac047afcc50fa2bd
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-07-09 20:17:57 -0700

    Create LRUcache.py

---
+++ Linked Lists/LRUcache.py
@@ -0,0 +1,52 @@
+class ListNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+
+        self.left, self.right = ListNode(0,0), ListNode(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev = prev
+        node.next = nxt
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
+        
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = ListNode(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
