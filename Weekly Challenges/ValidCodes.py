5576802e3c45f6a1f7866dd49c8667088fc60613
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-07-15 19:55:05 -0700

    Create ValidCodes.py and also gitignore

---
+++ Weekly Challenges/ValidCodes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        businesses = set(["electronics", "grocery", "pharmacy", "restaurant"])
+        valid_codes = []
+        for i in range(0, len(code)):
+            cur_code = code[i].replace("_", "A")
+            if cur_code != "" and cur_code.isalnum() == True and businessLine[i] in businesses and isActive[i] == True:
+                valid_codes.append([code[i], ord(businessLine[i][0])])
+
+        
+        valid_codes = sorted(valid_codes, key = lambda x: (x[1], x[0]))
+    
+        return [code[0] for code in valid_codes]
