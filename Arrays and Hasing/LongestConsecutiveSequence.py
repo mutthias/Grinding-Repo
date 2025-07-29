29e0807803237d2792e2516952e430ae60b01bf3
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-07-28 21:22:14 -0700

    Update LongestConsecutiveSequence.py

--- Arrays and Hasing/LongestConsecutiveSequence.py
+++ Arrays and Hasing/LongestConsecutiveSequence.py
@@ -1,24 +1,16 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-
-        if not nums:
-            return 0
-
+        numsSet = set(nums)
+        longest = 0
         
-        nums_set = set(nums)
-        nums_list = list(nums_set)
-        nums_list.sort()
-        res = 0
-        cur_len = 0
-        # 1 2 3 4 100 200
-        for i in range(1, len(nums_list)):
-            if nums_list[i] - 1 == nums_list[i - 1]:
-                cur_len += 1
-            else:
-                res = max(cur_len, res)
-                cur_len = 0
+        for num in numsSet:
+            if num - 1 not in numsSet:
+                length = 1
+                while num + length in numsSet:
+                    length += 1
+                longest = max(longest, length)
         
-        return max(res, cur_len) + 1
+        return longest
             
 
 
